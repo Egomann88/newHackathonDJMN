ba0c618a80bb888cec47c58dd3239c815f487996
Mateusz Wnuk <Mateusz.wnuk@bossinfo.ch>
2022-10-29 08:50:04 +0200

    add onSave func

--- Ferienplanner/src/components/Main.vue
+++ Ferienplanner/src/components/Main.vue
@@ -2,23 +2,23 @@
   <div class="bg-[#cbcbcb] py-4">
     <div class="flex flex-wrap overflow-hidden w-3/4 m-auto pt-2">
       <div class="w-1/3 px-4">
-        <form class="flex flex-wrap w-full overflow-hidden py-4 bg-gray-100 p-4" @submit="methode">
+        <form class="flex flex-wrap w-full overflow-hidden py-4 bg-gray-100 p-4" @submit="PostVacation(1)">
           <div class="w-full overflow-hidden py-4">
             <div class="w-full py-2 px-4 ">
               <div class="border-black border-2 rounded-sm">
                 <p class="text-center bg-[#519ad6] rounded-t-sm">Am/Ab*</p>
-                <input class="p-2 rounded-b-sm w-full" id="Test" type="date" required>
+                <input class="p-2 rounded-b-sm w-full" id="fromDate" name="from" type="date" required>
               </div>
             </div>
             <div v-if="showUntilInput" class="w-full py-2 px-4 ">
               <div class="border-black border-2 rounded-sm">
                 <p class="text-center bg-[#519ad6] rounded-t-sm">Bis (optional)</p>
-                <input class="p-2 rounded-b-sm w-full" id="Test2" type="date" required>
+                <input class="p-2 rounded-b-sm w-full" id="toDate" name="to" type="date">
               </div>
             </div>
           </div>
           <div class="w-full px-4 py-2">
-            <input class=" inline-block m-2" type="checkbox" @click="showUntilInput = !showUntilInput" />
+            <input class=" inline-block m-2" id="halfDAy" name="halfDay" type="checkbox" @click="showUntilInput = !showUntilInput" />
             <p class="inline-block">Halber Tag abwesend</p>
           </div>
           <div class="w-full px-4 py-2">
@@ -37,7 +37,7 @@
             </div>
           </div>
           <div class="w-full px-4">
-            <button class="button w-full my-4 bg-green-300 hover:bg-green-400" type="submit"
+            <button class="button w-full my-4 bg-green-300 hover:bg-green-400" @click="onSaveClick()" type="submit"
               value="submit">Bestätigen</button>
           </div>
         </form>
@@ -80,6 +80,8 @@ export default {
     let showUntilInput = true;
     let VacationAmount = 0;
     let PendingVacations = 0
+    let from = new Date("2000-01-01")
+    let to = new Date("2000-01-01")
 
     return {
       VacationAmount,
@@ -135,6 +137,19 @@ export default {
         });
       })
     },
+
+    onSaveClick: function() {
+      // strings
+      let fromDateValueStr = document.getElementById("fromDate").value;
+      let toDateValueStr = document.getElementById("toDate").value;
+      // date
+      let fromDateValueDate = fromDateValueStr;
+      let toDateValueDate = toDateValueStr;
+
+      alert("fromdate int " + toDateValueDate);
+      alert("todate int " + fromDateValueDate);
+    },
+
     GetVacationAmount: function(userID) {
       fetch('http://localhost:34474/API/user')
       .then(res => {
@@ -166,24 +181,46 @@ export default {
         })
       })
     },
-    PostVacationReason: async function() {
-      const response = await fetch("http://localhost:34474/api/vacationreason", {
-    method: 'POST',
-    body: `{
-   "VacationReason": Test,
-    }`,
-});
+    PostVacation: function(userID) {
+  //     let xhr = new XMLHttpRequest();
+  //   xhr.open("POST", "http://localhost:34474/api/vacation");
+  //   xhr.setRequestHeader("Accept", "application/json");
+  //   xhr.setRequestHeader("Content-Type", "application/json");
 
-response.json().then(data => {
-  console.log(data);
-});
+  // xhr.onreadystatechange = function () {
+  // if (xhr.readyState === 4) {
+  //   console.log(xhr.status);
+  //   console.log(xhr.responseText);
+  // }};
+  // let diffInDays = (this.to.getTime() - this.from.getTime()) / (1000 * 3600 * 24);
+  // alert(diffInDays)
+  // for(this.from; this.to <= this.from; this.from.getDate()++) {
+  //   alert(this.to);
+  //   alert(this.from);
+console.log(this.from)
+console.log(this.to)
+  for (this.form; this.from <= this.to; this.from.setDate(this.from.getDate() + 1)) {
+    console.log(this.from)
+}
+  
+
+  // let data = ` {
+  //       "ID": 1,
+  //       "UserID": 9,
+  //       "VacDayDate": "2022-06-21",
+  //       "IsFullDay": true,
+  //       "IsApproved": true,
+  //       "VacationReasonID": 2
+  //   }`;
+
+// xhr.send(data);
     }
   },
   mounted(){
     this.MarkUserVacationDates(6);
     this.GetVacationAmount(6);
     this.GetPendingVacationAmt(6);
-    this.PostVacationReason();
+    // this.PostVacation();
   }
 
 }
