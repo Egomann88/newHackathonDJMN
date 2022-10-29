c499b6f67ea163c1855161228219bb7fb90fc767
Justin <justin@kauz.ch>
2022-10-29 06:14:03 +0200

    login

--- Ferienplanner/src/components/Navbar.vue
+++ Ferienplanner/src/components/Navbar.vue
@@ -2,10 +2,10 @@
   <div>
     <section class="w-full mx-auto">
       <div class="flex flex-wrap w-full relative bg-[#427CAC] p-2">
-        <div class="w-full lg:w-1/3">
+        <div class="w-full lg:w-1/3 block">
           <img src="../assets/logo.svg" alt="Projekt Ferienplanner" class="h-full hidden lg:inline " />
         </div>
-        <div class="w-full lg:w-2/3 pl-20">
+        <div v-if="isLoggedIn()" class="w-full lg:w-2/3 pl-20">
           <router-link to="/" class="btn-Header m-2 inline-block text-lg">Kalendar
           </router-link>
           <router-link to="/statistic" class="btn-Header m-2 inline-block text-lg">Statistik
@@ -24,7 +24,12 @@ export default defineComponent({
   components: {},
   setup() { },
   mounted() { },
-  methods: {},
+  methods: {
+    isLoggedIn() {
+      if (localStorage.getItem('loggedIn') == '1') return true;
+      return false;
+    }
+  },
 });
 </script>
 
